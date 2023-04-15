128d77d67a85fb2124b6381d83465611f2627958
iuna Leon <yuna.ads@hotmail.com>
2023-04-15 20:47:43 -0300

    commitando

--- script.js
+++ script.js
@@ -24,15 +24,50 @@
 
 function adicionar(){
 
-    if(isNumero(num.value) && !inlista(num.value)){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))     //adicionar elementos no vetor
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)          //Para adicionar na lista
+        res.innerHTML = ''          //Quando eu adicionar um elemento ele vai limpar o resultado
 
     }else{
         window.alert('Valor inválido ou já encontrado na lista.')
     }
-   
+    num.value = ''
+    num.focus()    //apaga o numero que digitou automaticamente quando é inserido na lista
+ }
 
-     
+ function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar')
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+
+        for(let pos in valores){
+            if(valores[pos] > maior){
+                maior = valores[pos]
+            }if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+
+        media = soma / tot
+
+
+
+        res.innerHTML = ''      //para zerar as variáveis
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p> O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p> O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p> Somando todos os valores temos ${soma}.</p>`
+        res.innerHTML += `<p> A média dos valores digitados é ${media}.</p>`
     }
+ }
   
 
     
